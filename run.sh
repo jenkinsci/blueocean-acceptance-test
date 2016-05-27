f39866c037bbe9890f41461b9dfd440c3aeecc28
tfennelly <tom.fennelly@gmail.com>
2016-05-27 15:29:31 +0100

    DevRunner for easier dev of acceptance tests
    i.e. a --dev switch to start a clean Jenkins

--- run.sh
+++ run.sh
@@ -11,6 +11,10 @@
 #       e.g. ./run.sh -v=2.5 
 #   -s  Install local SNAPSHOTS. See https://github.com/jenkinsci/acceptance-test-harness/blob/master/docs/SUT-VERSIONS.md#install-plugins-from-local-maven-repository  
 #       e.g.  ./run.sh -s
+#   -d  Run a clean dev instance of Jenkins (with blueocean plugins) and keep it running. Allows you to do fast dev of tests in your IDE without having to
+#       constanly wait for Jenkins to startup etc. Need to ensure that your test does not use JenkinsAcceptanceTestRule while you are developing
+#       (e.g. via AbstractJUnitTest), or you'll be wasting your time.
+#       e.g.  ./run.sh -d
 # 
 #   All other args are assumed to be customs plugins ala https://github.com/jenkinsci/acceptance-test-harness/blob/master/docs/SUT-VERSIONS.md#use-custom-plugin-file
 #   e.g. ./run.sh blueocean-plugin.jpi=/Users/tfennelly/projects/blueocean/blueocean-plugin/target/blueocean-plugin.hpi
@@ -22,6 +26,8 @@ LOCAL_SNAPSHOTS=false
 PLUGINS=""
 AGGREGATOR_DIR=""
 AGGREGATOR_ENV=""
+DEV_JENKINS=false
+PROFILES="-P runTests"
 
 for i in "$@"
 do
@@ -35,6 +41,9 @@ case $i in
     -s|--snaps|--snapshots)
     LOCAL_SNAPSHOTS=true
     ;;
+    -d|--dev)
+    DEV_JENKINS=true    
+    ;;
     --default)
     ;;
     *)
@@ -42,6 +51,24 @@ case $i in
     ;;
 esac
 done
+
+if [ "${DEV_JENKINS}" == "true" ]; then
+    echo ""
+    echo "*****"
+    echo "***** Starting a test dev instance of Jenkins with the blueocean plugins."
+    echo "***** Watch console output for the URL to use while developing your test."
+    echo "*****"
+    echo "***** Be sure your test doesn't use JenkinsAcceptanceTestRule in any way"
+    echo "***** or you'll be defeating the purpose (e.g. via AbstractJUnitTest)."
+    echo "*****"
+    echo ""
+    
+    # Exclude the actual tests from this run and just run the skeleton test in
+    # ExcludedRunnerTest. It will just run with the plugins and stay running
+    # allowing you to iterate on your acceptance test dode without constantly
+    # having to restart Jenkins.
+    PROFILES="-P runDevRunner"
+fi
 
 # For now, the location of the aggregator plugin must be defined until we have
 # blueocean plugins in the Update Center.
@@ -70,6 +97,7 @@ echo "Running with switches:"
 echo "    --version=${JENKINS_VERSION}"
 echo "    --snapshots=${LOCAL_SNAPSHOTS}"
 echo "    --aggregator=${AGGREGATOR_DIR}"
+echo "    --dev=${DEV_JENKINS}"
 
 if [ "${PLUGINS}" == "" ]; then
     echo ""
@@ -92,7 +120,7 @@ if [ "${AGGREGATOR_DIR}" != "" ]; then
     AGGREGATOR_ENV="PLUGINS_DIR=${AGGREGATOR_DIR}/target/plugins"
 fi
 
-EXECUTION="env LOCAL_SNAPSHOTS=${LOCAL_SNAPSHOTS} ${PLUGINS} ${AGGREGATOR_ENV} JENKINS_WAR=./wars/jenkins-${JENKINS_VERSION}.war mvn test"
+EXECUTION="env LOCAL_SNAPSHOTS=${LOCAL_SNAPSHOTS} ${PLUGINS} ${AGGREGATOR_ENV} JENKINS_WAR=./wars/jenkins-${JENKINS_VERSION}.war mvn test ${PROFILES}"
 
 echo ""
 echo "> ${EXECUTION}"
---
+++ src/test/java/utils/DevRunner.java
@@ -0,0 +1,59 @@
+/*
+ * The MIT License
+ *
+ * Copyright (c) 2016, CloudBees, Inc.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package utils;
+
+import org.jenkinsci.test.acceptance.junit.AbstractJUnitTest;
+import org.jenkinsci.test.acceptance.junit.WithPlugins;
+import org.junit.Test;
+
+import java.io.PrintStream;
+
+/**
+ * @author <a href="mailto:tom.fennelly@gmail.com">tom.fennelly@gmail.com</a>
+ */
+public class DevRunner extends AbstractJUnitTest {
+    
+    @WithPlugins("workflow-aggregator@2.1")
+    @Test
+    public void runAndStayRunning() throws Exception {
+        System.out.println("");
+        System.out.println("------------------------------------------------------------------------------------");
+        System.out.println("    A clean dev instance of Jenkins is running now.");
+        System.out.println("     - " + jenkins.getCurrentUrl());
+        System.out.println("");
+        System.out.println("    You should now be able to develop tests against this instance without");
+        System.out.println("    having to constantly restart Jenkins.");
+        System.out.println("");
+        System.out.println("    NOTE:");
+        System.out.println("    Make sure your test does not use JenkinsAcceptanceTestRule while you are");
+        System.out.println("    developing (e.g. via AbstractJUnitTest), or you'll be wasting your time.");
+        System.out.println("------------------------------------------------------------------------------------");
+        System.out.println("");
+        System.out.println("ctrl-c to exit...");
+        
+        while(true) {
+            Thread.sleep(1000);
+        }
+    }
+}
