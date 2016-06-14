1da3bc2a0a477c7c87131ca55b9fcdb9c64cd0f2
tfennelly <tom.fennelly@gmail.com>
2016-06-14 17:52:31 +0900

    Don't start selenium

--- nightwatch.conf.js
+++ nightwatch.conf.js
@@ -7,6 +7,7 @@ module.exports = (function (settings) {
     }
     
     settings.test_settings.default.launch_url = fs.readFileSync(jenkins_url_file, 'utf8');
+    settings.selenium.start_process = false; // TODO: use a file or something to make this configurable - CI script could create it
     
     return settings;
 })(require('./src/main/nightwatch.json'));
