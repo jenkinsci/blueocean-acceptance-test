96e6879fe7dc8b8309112e2fa5b707fbb4b919ab
tfennelly <tom.fennelly@gmail.com>
2016-06-23 14:34:30 +0100

    console output of jenkins URL when running NightWatch

--- nightwatch.conf.js
+++ nightwatch.conf.js
@@ -5,8 +5,12 @@ module.exports = (function (settings) {
     if (!fs.existsSync(jenkins_url_file)) {
         throw 'Jenkins not running. Failed to find file: ' + jenkins_url_file;
     }
+
+    var launchUrl = fs.readFileSync(jenkins_url_file, 'utf8');
     
-    settings.test_settings.default.launch_url = fs.readFileSync(jenkins_url_file, 'utf8');
+    console.log('Jenkins running at: ' + launchUrl);
+    
+    settings.test_settings.default.launch_url = launchUrl;
 
     if (fs.existsSync('target/.selenium_server_provided')) {
         settings.selenium.start_process = false;
