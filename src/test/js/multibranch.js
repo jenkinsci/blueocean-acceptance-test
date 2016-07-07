ae0e57deb6d2c2de9f5524076ebe31360f35e74e
tfennelly <tom.fennelly@gmail.com>
2016-07-07 09:32:57 +0100

    stash

--- src/test/js/multibranch.js
+++ src/test/js/multibranch.js
@@ -1,10 +1,15 @@
-var git = require('../../main/js/api/git');
+const git = require('../../main/js/api/git');
+const path = require("path");
+
+const pathToRepo = path.resolve('./target/test-project');
+
+console.log('*** ' + pathToRepo);
 
 module.exports = {
     beforeEach: function (browser, done) {
-        git.createRepo('./src/test/resources/multibranch_1', './target/test-project')
+        git.createRepo('./src/test/resources/multibranch_1', pathToRepo)
             .then(function() {
-                git.createBranch('feature-1', './target/test-project')
+                git.createBranch('feature-1', pathToRepo)
                     .then(done);
             });
     },
