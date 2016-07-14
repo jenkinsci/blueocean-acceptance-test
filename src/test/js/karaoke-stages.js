66101e74b37341a0121b1621139ee9d7fbfd420c
Thorsten Scherler <scherler@gmail.com>
2016-07-14 23:46:46 +0200

    [JENKINS-36565] Add more tests and make the one we have more robust

--- src/test/js/karaoke-stages.js
+++ src/test/js/karaoke-stages.js
@@ -1,3 +1,6 @@
+const stringCleaner = function (string) {
+  return string.replace(/\r?\n|\r/g, '');
+};
 module.exports = {
     'Create Pipeline Job "stages"': function (browser) {
         const pipelinesCreate = browser.page.pipelineCreate().navigate();
@@ -30,12 +33,12 @@ module.exports = {
             .click('code')
             .keys(browser.Keys.UP_ARROW)
             .getText('code', function (result) {
-                const text = result.value;
+                const text = stringCleaner(result.value);
                 // we wait and see whether no more updates come through
                 this.pause(10)
                     .waitForElementVisible('code')
                     .getText('code', function (result) {
-                        this.assert.equal(text, result.value);
+                        this.assert.equal(text, stringCleaner(result.value));
                     });
 
             });
@@ -46,22 +49,12 @@ module.exports = {
 
     'Check Job Blue Ocean Pipeline run detail page - karaoke': function (browser) {
         const blueRunDetailPage = browser.page.bluePipelineRunDetail().forRun('stages', 'jenkins', 1);
-        blueRunDetailPage.waitForElementVisible('code')
-            .getText('code', function (result) {
-                const text = result.value;
-                //node change
-                this.waitForElementPresent('svg circle.success')
-                    .waitForElementVisible('code')
-                    .getText('code', function (result) {
-                        this.assert.notEqual(text, result.value)
-                    })
-                ;
-            })
-        ;
+        // if we have the first stage finished we are sure in karaoke mode
+        blueRunDetailPage.waitForElementPresent('svg circle.success');
         // FIXME should be taken from somewhere dynamically
         const nodeDetail =  blueRunDetailPage.forNode('5');
-        nodeDetail.waitForElementVisible('div.result-item')
-            .getText('div.result-item', function (result) {
+        nodeDetail.waitForElementVisible('span.result-item-label')
+            .getText('span.result-item-label', function (result) {
                 this.assert.equal('Shell Script', result.value);
             })
         ;
@@ -74,8 +67,33 @@ module.exports = {
             });
         // test whether the stage we seeing is highlighted
         nodeDetail.waitForElementVisible('g.pipeline-node-selected');
-        // wait for the success update via sse event
-        blueRunDetailPage.waitForElementVisible('div.header.success');
+        // test whether log lines are navigatable
+        // first turn on xpath to get the nodes we want
+        browser.useXpath();
+        var aXpath = '//code/p/a[1]';
+        nodeDetail
+            .waitForElementVisible(aXpath)
+            .getAttribute(aXpath, 'href', function (result) {
+                this.assert.equal(typeof result, "object");
+                this.assert.equal(result.status, 0);
+                const value = result.value;
+                this
+                    .click(aXpath)
+                    .url(function (response) {
+                        // did we changed the url on  change?
+                        this.assert.equal(response.value, result.value);
+                        // controll whether we can still see the log and the link is still the same
+                        this.waitForElementVisible(aXpath)
+                            .getAttribute(aXpath, 'href', function (inner) {
+                                this.assert.equal(inner.value, result.value);
+                            })
+                    })
+
+            });
+        // turn on css again
+        browser.useCss();
+        // wait for job to finish
+        nodeDetail.waitForElementVisible('div.header.success');
         browser.end();
     }
 };
--- src/test/js/test_pipelines_scripts/stages-with-wait.groovy
+++ src/test/js/test_pipelines_scripts/stages-with-wait.groovy
@@ -1,18 +1,19 @@
 node {
    stage 'Stage 1'
-   sh 'sleep 3; echo `date` Stage 1;sleep 3; echo `date` Stage 1;sleep 4;  '
+   sh 'sleep 3; echo `date` Stage 1;'
+   sh 'sleep 3; echo `date` Stage 1;'
 }
 node {
     stage 'Stage 2'
     parallel firstBranch: {
-        sh 'echo `date` Stage 2 - first;sleep 3; echo `date` Stage 2 - first;sleep 3; echo `date` Stage 2 - first;sleep 3; echo `date` Stage 2 - first;sleep 3; echo `date` Stage 2 - first;sleep 3; echo `date` Stage 2 - first;'
+        sh 'echo `date` Stage 2 - first;sleep 3; echo `date` Stage 2 - first;sleep 3; echo `date` Stage 2 - first;'
 
     }, secondBranch: {
-        sh 'echo `date` Stage 2 - second;sleep 3; echo `date` Stage 2 - second;sleep 3; echo `date` Stage 2 - second;sleep 3; echo `date` Stage 2 - second;sleep 3; echo `date` Stage 2 - second;sleep 3; echo `date` Stage 2 - second;'
+        sh 'echo `date` Stage 2 - second;sleep 3; echo `date` Stage 2 - second;sleep 3; echo `date` Stage 2 - second;'
     },
     failFast: true
 }
 node {
     stage 'fin'
-    sh 'echo `date` fin;sleep 3; echo `date` fin;sleep 3; echo `date` fin;sleep 3; echo `date` fin;sleep 3; echo `date` fin;sleep 3; echo `date` fin;'
+    sh 'echo `date` fin;sleep 3; echo `date` fin;'
 }
