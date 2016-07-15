fd4d224ee66e079b1b4f078d3d4cd2cd8930dd8d
tfennelly <tom.fennelly@gmail.com>
2016-07-15 17:28:26 +0100

    Debug logging of SSE events

--- src/main/js/globals.js
+++ src/main/js/globals.js
@@ -1,9 +1,11 @@
 var sse = require('./api/sse');
+var debug = require('./api/debug');
 
 module.exports = {
     waitForConditionTimeout: 20000,
     default: {
         beforeEach: function (browser, done) {
+            //debug.enable('sse');
             sse.connect(browser, done);
         },
         afterEach: function () {
