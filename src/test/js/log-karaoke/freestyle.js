54e8ff4b30f9ad3f9648c667c744a02dc7e91be4
Thorsten Scherler <scherler@gmail.com>
2016-07-20 22:17:59 +0200

    [master] Change test to test positive. stop karaoke via keyUp is tested in noStage

--- src/test/js/log-karaoke/freestyle.js
+++ src/test/js/log-karaoke/freestyle.js
@@ -20,30 +20,14 @@ module.exports = {
         blueActivityPage.waitForRunRunningVisible('hijo-1');
     },
 
-    // need to click on an element so the up_arrow takes place in the window
-    'Check Job Blue Ocean run detail page - stop karaoke': function (browser) {
+    'Check Job Blue Ocean run detail page - karaoke': function (browser) {
         const blueRunDetailPage = browser.page.bluePipelineRunDetail().forRun('hijo', 'jenkins', 1);
+        // assert basic page style
         blueRunDetailPage.assertBasicLayoutOkay();
 
-        // The log appears in the <code> window, of which there an be only one.
-        // Click on it to focus it so we make sure the key up is fired on the page and
-        // not directly on the browser
-        browser.waitForElementVisible('code')
-            .click('code');
-
-        // Press the up-arrow key to tell karaoke mode to stop following the log i.e.
-        // after this point in time, the content of the <code> block should not change.
-        browser.keys(browser.Keys.UP_ARROW);
-
-        // So, because we have pressed the up-arrow (see above), the log karaoke
-        // should stop. So if we now wait for the build to end, we should NOT see
-        // the log output generated at the end of freestyle.sh i.e. the "freeStyle end"
-        // string. If we see that string, that means that karaoke did not stop and
-        // something is wrong with the up-arrow listener.
-        browser.expect.element('code').text.to.not.contain('freeStyle end');
         blueRunDetailPage.waitForJobRunEnded('hijo')
             .waitForElementVisible('code')
-            // .expect.element('code').text.to.not.contain('freeStyle end'); -> that is failing on the server only
+            .expect.element('code').text.to.contain('freeStyle end');
     },
 
 };
