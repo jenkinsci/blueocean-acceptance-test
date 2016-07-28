5a3abb5727c0ca8c08b8668102f1f5a8dfe51793
Thorsten Scherler <scherler@gmail.com>
2016-07-28 13:10:07 +0200

    Jenkins 36131 "Show all" link required anywhere a log is displayed (#21)
    * Debug logging of SSE events
    
    * More on logging (client and server side)
    
    * Better SSE connection disconnect cleanup
    
    * Update to latest SSE gateway client api
    
    * Use a relative path to logging.properties file
    
    * Remove misleading seleium docs in readme (#11)
    
    * [master] Fix last test by turning on cssPath again before finishing last test
    
    * [master] try to fix freestyle error validation on server
    
    * [master] comment the flaky test until I come up with something better. stop karaoke is covered in other tests as well
    
    * [master] Add more comments to code
    
    * [master] Change test to test positive. stop karaoke via keyUp is tested in noStage
    
    * [JENKINS-36131] add test coverage for full log in freestyle and noStages. stages are covered by noStages test
    
    * [JENKINS-36131] refactor clickFullLog to be part of the bPRDetail page, reduces all biolerplate in the tests
    
    * [JENKINS-36131] Add test whether the fullLogButton is not present and exposed it as function on detail page.

--- src/test/resources/test_scripts/freestyle.sh
+++ src/test/resources/test_scripts/freestyle.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env bash
 echo `date` freeStyle start;
 sleep 2;
 echo `date` step 1;
@@ -5,6 +6,10 @@ sleep 1;
 echo `date` step 2;
 sleep 2;
 echo `date` step 3;
-sleep 3;
+sleep 2;
 echo `date` freeStyle end;
-sleep 4;
+COUNTER=0
+while [  $COUNTER -lt 10001 ]; do
+ echo The counter is $COUNTER
+ let COUNTER=COUNTER+1
+done
