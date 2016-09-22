eb507559dc0f93b4b60f8a2097e57e8b8d3535ad
tfennelly <tom.fennelly@gmail.com>
2016-09-22 18:44:24 +0100

    Better messages on the start stop scripts

--- start-selenium.sh
+++ start-selenium.sh
@@ -3,7 +3,7 @@
 ./stop-selenium.sh
 
 echo ""
-echo " Starting container..."
+echo " Starting Selenium Docker container..."
 echo ""
 docker run -d --name blueo-selenium -p 15900:5900 -p 4444:4444 -e no_proxy=localhost selenium/standalone-firefox-debug:2.53.0
 
--- stop-selenium.sh
+++ stop-selenium.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 echo ""
-echo " Zapping old container, if any. This may take a few seconds..."
+echo " Zapping old Selenium Docker container, if any. This may take a few seconds..."
 echo ""
 docker stop blueo-selenium && docker rm blueo-selenium
