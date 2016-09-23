328365bcef0648a48961fe734bbec5fb8637e981
tfennelly <tom.fennelly@gmail.com>
2016-09-23 12:35:52 +0100

    Cleaned up messages from selenium-docker start scripts

--- start-selenium.sh
+++ start-selenium.sh
@@ -5,14 +5,17 @@
 echo ""
 echo " Starting Selenium Docker container..."
 echo ""
-docker run -d --name blueo-selenium -p 15900:5900 -p 4444:4444 -e no_proxy=localhost selenium/standalone-firefox-debug:2.53.0
+docker run -d --name blueo-selenium -p 15900:5900 -p 4444:4444 -e no_proxy=localhost selenium/standalone-firefox-debug:2.53.0 > /dev/null
 
 # Output the containers bridge network IP to file
-docker inspect -f '{{ .NetworkSettings.IPAddress }}' blueo-selenium > ./target/.selenium_ip
+SELENIUM_IP=`docker inspect -f '{{ .NetworkSettings.IPAddress }}' blueo-selenium`
+echo $SELENIUM_IP > ./target/.selenium_ip
 
 echo ""
 echo "**************************************************************"
 echo "**** Docker container with Selenium, Firefox and VNC running."
-echo "****   To connect and view, run:"
+echo "****   Selenium server listening on $SELENIUM_IP:4444"
+echo "****   "
+echo "****   To connect and view with VNC, run:"
 echo "****   $ open vnc://:secret@localhost:15900"
 echo "**************************************************************"
--- stop-selenium.sh
+++ stop-selenium.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 echo ""
-echo " Zapping old Selenium Docker container, if any. This may take a few seconds..."
+echo " Stopping old Selenium Docker container, if any. This may take a few seconds..."
+docker stop blueo-selenium > /dev/null
+docker rm blueo-selenium > /dev/null
+echo "      ... stopped"
 echo ""
-docker stop blueo-selenium && docker rm blueo-selenium
