68d880deb254482000f308168ec1f563423f4b1a
Tom Fennelly <tom.fennelly@gmail.com>
2016-09-23 11:16:07 +0100

    export the selenium server container IP address

--- start-selenium.sh
+++ start-selenium.sh
@@ -6,6 +6,7 @@ echo ""
 echo " Starting Selenium Docker container..."
 echo ""
 docker run -d --name blueo-selenium -p 15900:5900 -p 4444:4444 -e no_proxy=localhost selenium/standalone-firefox-debug:2.53.0
+export BLUEO_SELENIUM_SERVER_ADDR=`docker inspect -f '{{ .NetworkSettings.IPAddress }}' blueo-selenium`
 
 echo ""
 echo "**************************************************************"
