164b601d935e555715ae77f31b1c4ccf80f0728c
Ivan Meredith <ivan@ivan.net.nz>
2016-11-24 14:25:21 +1300

    jsdoc changes

--- src/test/js/freestyle/index.js
+++ src/test/js/freestyle/index.js
@@ -1,8 +1,8 @@
-/** @module nonmultibranch
+/** @module freestyle
  * @memberof testcases
  * @description We are testing different REGRESSIONS that had appeared, so they do not appear again.
  *
- * @see {@link module:nonmultibranch.freestyle}
+ * @see {@link module:freestyle.activityTabs}
  */
 module.exports = {
 
