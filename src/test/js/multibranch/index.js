bad9cf18c3f9d75fb546ccb1c3d06fd31655e15f
Thorsten Scherler <scherler@gmail.com>
2016-11-26 02:17:28 +0100

    [JENKINS-40027_REGRESSION_cant_open_branches_that_have_/_in_the_name] slim down tests

---
+++ src/test/js/multibranch/index.js
@@ -0,0 +1,9 @@
+/** @module multibranch
+ * @memberof testcases
+ * @description We are testing different REGRESSIONS that had appeared, so they do not appear again.
+ *
+ * @see {@link module:multibranch.multibranchOpening}
+ */
+module.exports = {
+
+};
--- src/test/js/multibranch/multibranchOpening.js
+++ src/test/js/multibranch/multibranchOpening.js
@@ -1,75 +1,67 @@
-const jobName = 'withGitFlow';
+/** @module multibranchOpening
+ * @memberof multibranch
+ * @description TEST: basic tests around multiBranch and encoding of branches
+ */
+const async = require('async');
 const path = require('path');
 const pathToRepo = path.resolve('./target/test2-project-folder');
-const soureRep = './src/test/resources/multibranch_2';
+const sourceRep = './src/test/resources/multibranch_2';
 const git = require('../../../main/js/api/git');
-const async = require('async');
-const pageHelper = require('../../main/js/util/pageHelper');
+const pageHelper = require('../../../main/js/util/pageHelper');
 const createCallbackWrapper = pageHelper.createCallbackWrapper;
 
 module.exports = {
-
-    // ** creating a git repo */
-    before: function (browser, done) {
-          // we creating a git repo in target based on the src repo (see above)
-          git.createRepo(soureRep, pathToRepo)
-              .then(function () {
-                  git.createBranch('feature/1', pathToRepo)
-                      .then(done);
-              });
-    },
-
+  // ** creating a git repo */
+  before: function (browser, done) {
+    // we creating a git repo in target based on the src repo (see above)
+    git.createRepo(sourceRep, pathToRepo)
+      .then(function () {
+        git.createBranch('feature/1', pathToRepo)
+          .then(done);
+      });
+  },
   /**
-   * Make sure we can open the master and the feature/1 branch results screen from activity
+   * Make sure we can open the master and the feature/1 branch results screen from activity and branch tab
    * Regression: https://issues.jenkins-ci.org/browse/JENKINS-40027
    */
-    'open master and feature/1 branch from activity': function (browser) {
-        const cases = [{
-            regexp: 'tr[id^="master"]',
-            name: 'masterActivityMB',
-          }, {
-            regexp: 'tr[id^="feature"]',
-            name: 'featureActivityMB',
-          },
-        ];
-        async.mapSeries(cases, function (usecase, callback) {
-           const jobname = usecase.name;
-           const regexp = usecase.regexp;
-           const multibranchCreate = browser.page.multibranchCreate().navigate();
-           multibranchCreate.createBranch(jobName, pathToRepo);
-           multibranchCreate.createBranch(jobname, pathToRepo);
-           const blueActivityPage = browser.page.bluePipelineActivity().forJob(jobName, 'jenkins');
-           blueActivityPage.waitForElementVisible(regexp);
-           blueActivityPage.click(regexp);
-           browser.page.bluePipelineRunDetail().assertMultiBranchResult(createCallbackWrapper(callback));
+  'open master and feature/1 branch from activity and from branch tab': function (browser) {
+    const cases = [{
+        regexp: 'tr[id^="master"]',
+        name: 'masterActivityMB',
+      }, {
+        regexp: 'tr[id^="feature"]',
+        name: 'featureActivityMB',
+      },
+      {
+        regexp: 'tr[id^="master"]',
+        name: 'masterBranchesMB',
+        branch: true,
+      }, {
+        regexp: 'tr[id^="feature"]',
+        name: 'featureBranchesMB',
+        branch: true,
+      },
+    ];
+    async.mapSeries(cases, function (usecase, callback) {
+      const jobName = usecase.name;
+      const regexp = usecase.regexp;
+      const branch = usecase.branch;
+      console.log('creating job', jobName);
+      const multiBranchCreate = browser.page.multibranchCreate().navigate();
+      multiBranchCreate.createBranch(jobName, pathToRepo);
+      const blueActivityPage = browser.page.bluePipelineActivity().forJob(jobName, 'jenkins');
+      console.log('clicking from' , branch ? 'branches' : 'activities', 'tab');
+      function detailCheck() {
+        blueActivityPage.waitForElementVisible(regexp);
+        blueActivityPage.click(regexp, function () {
+          browser.page.bluePipelineRunDetail().assertMultiBranchResult(createCallbackWrapper(callback));
         });
-    },
-
-
-    /**
-     * Make sure we can open the master and the feature/1 branch results screen from branch
-     * Regression: https://issues.jenkins-ci.org/browse/JENKINS-40027
-     */
-    'open master branch from branches tab': function (browser) {
-        const cases = [{
-            regexp: 'tr[id^="master"]',
-            name: 'masterBranchesMB',
-          }, {
-            regexp: 'tr[id^="feature"]',
-            name: 'featureBranchesMB',
-          },
-        ];
-        async.mapSeries(cases, function (usecase, callback) {
-           const jobname = usecase.name;
-           const regexp = usecase.regexp;
-           const multibranchCreate = browser.page.multibranchCreate().navigate();
-           multibranchCreate.createBranch(jobName, pathToRepo);
-           multibranchCreate.createBranch(jobname, pathToRepo);
-           const blueActivityPage = browser.page.bluePipelineActivity().forJob(jobName, 'jenkins');
-           blueActivityPage.click(".branches");
-           browser.waitForElementVisible(regexp);
-           browser.click(regexp);
-           browser.page.bluePipelineRunDetail().assertMultiBranchResult(createCallbackWrapper(callback));
-        });
-    },
+      }
+      if (branch) {
+        blueActivityPage.click(".branches", detailCheck);
+      } else {
+        detailCheck();
+      }
+    });
+  },
 };
