ffc8ef630e57cb7e13bffa6464e8edcc05a97cc1
Michael Neale <michael.neale@gmail.com>
2016-11-30 11:29:55 -0800

    test i18n by loading up german

---
+++ src/test/js/i18n/loadGerman.js
@@ -0,0 +1,25 @@
+const jobName = 'loadGerman';
+/** @module loadGerman
+ * @memberof i18n
+ * @description basic smoke test for i18n
+ */
+module.exports = {
+    /** Load it in German */
+    'Step 01': function (browser) {
+        var bluePipelines = browser.page.bluePipelinesDE().navigate();
+        bluePipelines.waitForElementVisible('.btn-secondary.inverse');
+        browser.getText('.btn-secondary.inverse', function(response) {            
+            browser.assert.equal(response.value, 'Neue Pipeline');
+        });        
+    },
+
+    /** Load it in The Queens English, God Bless The Queen */
+    'Step 01': function (browser) {
+        var bluePipelines = browser.page.bluePipelines().navigate();
+        bluePipelines.waitForElementVisible('.btn-secondary.inverse');
+        browser.getText('.btn-secondary.inverse', function(response) {            
+            browser.assert.equal(response.value, 'New Pipeline');
+        });        
+    },
+    
+};
