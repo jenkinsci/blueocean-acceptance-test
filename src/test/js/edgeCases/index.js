3e4364d7df743f7835b0a5d1f11b292ad2618ac5
tfennelly <tom.fennelly@gmail.com>
2016-12-01 15:51:28 -0800

    Revert "[FIX-JENKINS-39229_Regression_Initial_stage_run_does_not_show_graph] add … (#79)"
    This reverts commit 28064f2c9556826e4748c245ae0f26c95dbbc981.

--- src/test/js/edgeCases/index.js
+++ src/test/js/edgeCases/index.js
@@ -3,7 +3,6 @@
  * @description We are testing different REGRESSIONS that had appeared, so they do not appear again.
  *
  * @see {@link module:edgeCases.folder}
- * @see {@link module:edgeCases.initialStage}
  */
 module.exports = {
 
