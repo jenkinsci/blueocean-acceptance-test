bc833bbb2584be490d6bf2099d6b305f90ce3357
Michael Neale <michael.neale@gmail.com>
2016-12-08 11:01:51 +1100

    relax the timing a bit as often jobs are finished (#84)
    * relax the timing a bit as often jobs are finished
    
    * Update stages-with-wait-pipelineModel-syntax.groovy
    
    * Update stages-with-wait-block-syntax.groovy
    
    * Update stages-with-wait.groovy

--- src/test/resources/test_scripts/stages-with-wait-block-syntax.groovy
+++ src/test/resources/test_scripts/stages-with-wait-block-syntax.groovy
@@ -5,15 +5,15 @@ node {
    }
     stage ('Stage 2'){
         parallel firstBranch: {
-            sh 'echo `date` Stage 2 - first;sleep 3; echo `date` Stage 2 - first;sleep 3; echo `date` Stage 2 - first;'
+            sh 'echo `date` Stage 2 - first;sleep 3; echo `date` Stage 2 - first;sleep 5; echo `date` Stage 2 - first;'
 
         }, secondBranch: {
-            sh 'echo `date` Stage 2 - second;sleep 3; echo `date` Stage 2 - second;sleep 3; echo `date` Stage 2 - second;'
+            sh 'echo `date` Stage 2 - second;sleep 3; echo `date` Stage 2 - second;sleep 5; echo `date` Stage 2 - second;'
         },
         failFast: true
     }
     stage ('fin'){
-        sh 'echo `date` fin;sleep 3; echo `date` fin;'
+        sh 'echo `date` fin;sleep 6; echo `date` fin;'
         sh 'echo yeah > foo.txt'
         archiveArtifacts 'foo.txt'
     }
