ce190beeb3685b133bd638c357aac612fe138f66
Cliff Meyers <cmeyers@cloudbees.com>
2016-12-29 10:20:46 -0500

    [JENKINS-40256] use a semantic selector

--- src/test/js/i18n/loadGerman.js
+++ src/test/js/i18n/loadGerman.js
@@ -1,4 +1,6 @@
 const jobName = 'loadGerman';
+const newButtonSelector = '.btn-new-pipeline';
+
 /** @module loadGerman
  * @memberof i18n
  * @description basic smoke test for i18n
@@ -7,8 +9,8 @@ module.exports = {
     /** Load it in German */
     'Step 01': function (browser) {
         var bluePipelines = browser.page.bluePipelines().navigateLanguage("de");
-        bluePipelines.waitForElementVisible('.btn-secondary.inverse');
-        browser.getText('.btn-secondary.inverse', function(response) {            
+        bluePipelines.waitForElementVisible(newButtonSelector);
+        browser.getText(newButtonSelector, function(response) {
             browser.assert.equal(response.value, 'Neue Pipeline');
         });        
     },
@@ -16,8 +18,8 @@ module.exports = {
     /** Load it in The Queens English, God Bless The Queen */
     'Step 02': function (browser) {
         var bluePipelines = browser.page.bluePipelines().navigateLanguage("en");
-        bluePipelines.waitForElementVisible('.btn-secondary.inverse');
-        browser.getText('.btn-secondary.inverse', function(response) {            
+        bluePipelines.waitForElementVisible(newButtonSelector);
+        browser.getText(newButtonSelector, function(response) {
             browser.assert.equal(response.value, 'New Pipeline');
         });        
     },
