d2e2fb9d607e908e8a7fa27f4d3dcb332685c008
Michael Neale <michael.neale@gmail.com>
2017-02-01 18:56:27 +1100

    bypasses a test race condition

--- src/test/js/multibranch/testResults.js
+++ src/test/js/multibranch/testResults.js
@@ -27,9 +27,14 @@ module.exports = {
     },
 
     'Open acitivty page wait for first run to finish': (browser) => {
+        
+
         const blueActivityPage = browser.page.bluePipelineActivity().forJob(jobName);
+        blueActivityPage.click(".branches");
+        blueActivityPage.click(".activity");
         // validate that we have 3 activities from the previous tests
         blueActivityPage.assertActivitiesToBeEqual(1);
+        
 
         blueActivityPage.waitForRunUnstableVisible(`${jobName}-1`)
     },
