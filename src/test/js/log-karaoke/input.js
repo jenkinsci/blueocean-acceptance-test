5505c9b182e5ddea2138e40237cc36acda822847
Michael Neale <michael.neale@gmail.com>
2017-02-08 10:17:56 +1100

    eliminate some problematic waits and use "new" retry feature (#117)
    Merging this in to get things in a reasonable state (we restoring the events when we have solved SSE will be easy).

--- src/test/js/log-karaoke/input.js
+++ src/test/js/log-karaoke/input.js
@@ -25,9 +25,9 @@ module.exports = {
     'Step 03': function (browser) {
         const blueActivityPage = browser.page.bluePipelineActivity().forJob(jobName, 'jenkins');
         // Check the run is turning to pause
-        blueActivityPage.waitForJobRunPaused(jobName, function () {
-            blueActivityPage.waitForRunPausedVisible(jobName + '-1');
-        });
+        blueActivityPage.waitForElementVisible('.circle-bg.paused');        
+        blueActivityPage.waitForRunPausedVisible(jobName + '-1');
+        
     },
     /** Check Job Blue Ocean Pipeline Activity Page has run  - then go to the detail page and validate the input form
      * */
@@ -38,9 +38,8 @@ module.exports = {
         // submit the form
         blueRunDetailPage.click('@inputStepSubmit');
         // Check the run is turning to unpaused
-        blueRunDetailPage.waitForJobRunUnpaused(jobName, function () {
-            // wait for job to finish
-            blueRunDetailPage.waitForJobRunEnded(jobName);
-        });
+        
+        blueRunDetailPage.waitForElementNotPresent('.circle-bg.paused');
+        blueRunDetailPage.waitForElementVisible('.BasicHeader--success');
     }
 };
