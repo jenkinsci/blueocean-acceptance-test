6e0f3107b1684220a459cf9ae65a8090f287994a
Michael Neale <michael.neale@gmail.com>
2017-02-23 09:26:24 +1100

    Enhancement/cover branch filtering (#121)
    * test for the new branch filtering
    
    * change the name of the project folder for testing

---
+++ src/test/js/multibranch/filtering.js
@@ -0,0 +1,55 @@
+const jobName = 'withGitFlow';
+const path = require("path");
+const pathToRepo = path.resolve('./target/testfilter-project-folder');
+const soureRep = './src/test/resources/multibranch_2';
+const git = require("../../../main/js/api/git");
+
+/** @module filtering
+ * @memberof multibranch
+ * @description Check that can filter the activity list
+ */
+module.exports = {
+
+
+    // ** creating a git repo */
+    before: function (browser, done) {
+          // we creating a git repo in target based on the src repo (see above)
+          git.createRepo(soureRep, pathToRepo)
+              .then(function () {
+                  git.createBranch('feature/1', pathToRepo)
+                      .then(done);
+              });
+    },
+    
+    /**
+     * Make sure we can open the master branch results screen from activity
+     */
+    'open master branch from activity': function (browser) {
+
+         var jobName = "filterActivityMB";      
+         var multibranchCreate = browser.page.multibranchCreate().navigate();      
+         multibranchCreate.createBranch(jobName, pathToRepo);
+
+         var blueActivityPage = browser.page.bluePipelineActivity().forJob(jobName, 'jenkins');
+         
+         blueActivityPage.waitForElementVisible('input.autocomplete');         
+         blueActivityPage.click('input.autocomplete');
+         blueActivityPage.waitForElementVisible('.item');                                    
+         
+         browser.elements('css selector', 'tbody tr', function(res) {
+           browser.assert.equal(2, res.value.length, 'Correct number of runs filtered down');          
+         })
+
+         
+         blueActivityPage.click('.item');                                    
+         
+         browser.elements('css selector', 'tbody tr', function(res) {
+           browser.assert.equal(1, res.value.length, 'Correct number of runs filtered down');          
+         })
+         
+         
+    },
+    
+    
+
+}
